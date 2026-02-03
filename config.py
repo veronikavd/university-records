a403796b74ffcab51c717c2ff201a73245f7fe16
veronikavd <veronika.lutsak.23@pnu.edu.ua>
2026-02-03 21:25:07 +0100

    Хмара

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
 
 class Config:
-    SECRET_KEY = 'university-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/university_db'
+    SECRET_KEY = 'cloud-pnu-key-secret'
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://2oZRFvXxebPBHiU.root:9bYsFfEF8BAzaI42@gateway01.eu-central-1.prod.aws.tidbcloud.com:4000/university_db?ssl_verify_cert=true&ssl_verify_identity=true'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_recycle": 280,
+        "pool_pre_ping": True
+    }
